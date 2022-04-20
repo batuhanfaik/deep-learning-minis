ae2db41a57ed830f0ceddcf6685fbce67a4c8716
batuhanfaik <batuhanfaik@gmail.com>
2022-04-20 23:23:50 +0200

    Implemented training loop

--- Miniproject_1/model.py
+++ Miniproject_1/model.py
@@ -2,27 +2,50 @@ from typing import Optional
 from utils import GORA
 
 import torch
-
-
-def __get_loss_fn(loss_fn: Optional[None, str] = None) -> torch.nn.modules.loss:
-    if loss_fn is None or loss_fn == 'l2':
-        return torch.nn.MSELoss()
-    elif loss_fn == 'l1':
-        return torch.nn.L1Loss()
-    else:
-        raise ValueError(f'Unknown loss function {loss_fn}')
+import time
 
 
 class Model:
     def __init__(self) -> None:
+        # Initialize model
         self.model = GORA()
-        raise NotImplementedError
+        # Set the parameters
+        self.optimizer = self.__get_optimizer()
+        self.loss_fn = self.__get_loss_fn()
+        self.n_epochs = 100
+        self.batch_size = 32
 
     def load_pretrained_model(self) -> None:
         raise NotImplementedError
 
     def train(self, train_input, train_target) -> None:
-        raise NotImplementedError
+        # Set model in training mode
+        self.model.train()
+        # Training loop
+        start_time = time.time()
+        for epoch in range(self.n_epochs):
+            print(f'Epoch {epoch + 1} / {self.n_epochs}')
+            # Minibatch loop
+            for batch_idx in range(0, len(train_input), self.batch_size):
+                # Get minibatch
+                batch_input = train_input[batch_idx:batch_idx + self.batch_size]
+                batch_target = train_target[batch_idx:batch_idx + self.batch_size]
+                # Zero the gradients
+                self.optimizer.zero_grad()
+                # Forward pass
+                output = self.model(batch_input)
+                # Compute loss
+                loss = self.loss_fn(output, batch_target)
+                # Backward pass
+                loss.backward()
+                # Update parameters
+                self.optimizer.step()
+                # Print loss
+                if batch_idx % 10 == 0:
+                    print(
+                        f'\tBatch {batch_idx + 1} / {len(train_input)}: {loss.item():.4f}')
+        end_time = time.time()
+        print(f'Training time: {end_time - start_time:.2f}s')
 
     def predict(self, test_input) -> torch.Tensor:
         raise NotImplementedError
@@ -36,3 +59,12 @@ class Model:
             betas=(0.9, 0.99),
             eps=1e-08,
         )
+
+    @staticmethod
+    def __get_loss_fn(loss_fn: Optional[None, str] = None) -> torch.nn.modules.loss:
+        if loss_fn is None or loss_fn == 'l2':
+            return torch.nn.MSELoss()
+        elif loss_fn == 'l1':
+            return torch.nn.L1Loss()
+        else:
+            raise ValueError(f'Unknown loss function {loss_fn}')
