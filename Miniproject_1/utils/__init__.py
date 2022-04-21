6f97c50f25f29a205158fb0ce99e2c8f4f0c29fe
batuhanfaik <batuhanfaik@gmail.com>
2022-04-21 13:25:50 +0200

    Implemented PSNR

--- Miniproject_1/utils/__init__.py
+++ Miniproject_1/utils/__init__.py
@@ -1,1 +1,2 @@
+from utils import *
 from gora import GORA
---
+++ Miniproject_1/utils/utils.py
@@ -0,0 +1,17 @@
+import torch
+
+
+def psnr(denoised_image: torch.Tensor, original_image: torch.Tensor) -> float:
+    """
+    Computes the PSNR between two images.
+
+    Args:
+        denoised_image: the denoised image
+        original_image: the original image
+
+    Returns:
+        the PSNR between the two images
+    """
+    mse = torch.mean((denoised_image - original_image) ** 2)
+    eps = 1e-8
+    return -10 * torch.log10(mse + eps)
