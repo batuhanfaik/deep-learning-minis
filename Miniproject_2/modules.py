5925214415375bdc3bd4a02f29c08a7375f8cef5
batuhanfaik <batuhanfaik@gmail.com>
2022-05-21 13:46:05 +0200

    Started ConvTranspose2d (boilerplate)

--- Miniproject_2/modules.py
+++ Miniproject_2/modules.py
@@ -1,4 +1,3 @@
-
 import torch
 
 from tensor import make_gtensor
@@ -7,11 +6,12 @@ from parameter import Parameter
 from functional import linear, relu, sigmoid
 from utils import check_inputs, get_gradient
 
+
 class Sequential(Module):
     def __init__(self, *modules) -> None:
         super().__init__('Sequential')
         self.modules = modules
-    
+
     def forward(self, *input):
         check_inputs(input)
         output = input[0]
@@ -26,7 +26,7 @@ class Sequential(Module):
 
         for module in self.modules[::-1]:
             output = module.backward(output)
-        
+
         return output
 
 
@@ -41,13 +41,14 @@ class Linear(Module):
         self.input = None
         self.register_parameter('weight', self.weight)
         self.register_parameter('bias', self.bias)
-    
+
     def forward(self, *input):
         check_inputs(input)
         self.input = input[0]
-        output = make_gtensor(linear(self.input, self.weight.data, self.bias.data), self, self.input)
+        output = make_gtensor(linear(self.input, self.weight.data, self.bias.data),
+                              self, self.input)
         return output
-    
+
     def backward(self, *gradwrtoutput):
         grad = get_gradient(gradwrtoutput)
         weight_grad = torch.mm(grad.T, self.input)
@@ -57,7 +58,46 @@ class Linear(Module):
 
         if self.bias is not None:
             self.bias.accumulate_grad(bias_grad)
-        
+
+        return input_grad
+
+
+class ConvTranspose2d(Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0,
+                 output_padding=0, groups=1, bias=True, dilation=1,
+                 padding_mode='zeros', device=None, dtype=None) -> None:
+        super().__init__('ConvTranspose2d')
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.output_padding = output_padding
+        self.groups = groups
+        self.weight = Parameter(torch.empty((out_dim, in_dim)))
+        self.bias = Parameter(torch.empty(out_dim)) if bias else None
+        self.dilation = dilation
+        self.padding_mode = padding_mode
+        self.device = device
+        self.dtype = dtype
+
+    def forward(self, *input):
+        check_inputs(input)
+        self.input = input[0]
+        output = make_gtensor(linear(self.input, self.weight.data, self.bias.data),
+                              self, self.input)
+        return output
+
+    def backward(self, *gradwrtoutput):
+        grad = get_gradient(gradwrtoutput)
+        weight_grad = torch.mm(grad.T, self.input)
+        bias_grad = grad
+        input_grad = torch.mm(grad, self.weight.data)
+        self.weight.accumulate_grad(weight_grad)
+
+        if self.bias is not None:
+            self.bias.accumulate_grad(bias_grad)
+
         return input_grad
 
 
@@ -65,13 +105,13 @@ class ReLU(Module):
     def __init__(self) -> None:
         super().__init__("ReLU")
         self.input = None
-    
+
     def forward(self, *input):
         check_inputs(input)
         self.input = input[0]
         output = make_gtensor(relu(self.input), self, self.input)
         return output
-    
+
     def backward(self, *gradwrtoutput):
         grad = get_gradient(gradwrtoutput)
         input_grad = grad * torch.where(self.input > 0, 1, 0)
@@ -82,17 +122,17 @@ class Sigmoid(Module):
     def __init__(self) -> None:
         super().__init__("Sigmoid")
         self.input = None
-    
+
     def forward(self, *input):
         check_inputs(input)
         self.input = input[0]
         output = make_gtensor(sigmoid(self.input), self, self.input)
         return output
-    
+
     def backward(self, *gradwrtoutput):
         grad = get_gradient(gradwrtoutput)
         input_sigmoid = sigmoid(self.input)
-        input_grad = grad * input_sigmoid * (1-input_sigmoid)
+        input_grad = grad * input_sigmoid * (1 - input_sigmoid)
         return input_grad
 
 
@@ -102,7 +142,7 @@ class MSELoss(Module):
         self.reduction = reduction
         self.input = None
         self.target = None
-    
+
     def forward(self, *input):
         check_inputs(input, 2)
         self.input = input[0]
@@ -114,14 +154,14 @@ class MSELoss(Module):
             loss = torch.sum(error, dim=0)
         elif self.reduction == "mean":
             loss = torch.mean(error, dim=0)
-        
+
         return make_gtensor(loss, self, [self.input, self.target])
-    
+
     def backward(self, *gradwrtoutput):
         grad = get_gradient(gradwrtoutput)
         input_grad = 2 * (self.input - self.target)
 
         if self.reduction == "mean":
             input_grad = input_grad / len(self.input)
-        
+
         return grad * input_grad
--- Miniproject_2/tests/test_modules.py
+++ Miniproject_2/tests/test_modules.py
@@ -1,20 +1,36 @@
 import torch
 import unittest
 
-from modules import Linear, ReLU, Sigmoid, Sequential, MSELoss
+from modules import Linear, ConvTranspose2d, ReLU, Sigmoid, Sequential, MSELoss
 from tensor import GTensor
+
 
 class TestModules(unittest.TestCase):
     def test_linear(self):
         x = torch.tensor([[1.0, 1.0], [1.0, 1.0]])
         linear = Linear(2, 4)
         out = linear.forward(x)
-        self.assertTrue(torch.equal(torch.mm(x, linear.weight.data.T) + linear.bias.data, out))
+        self.assertTrue(
+            torch.equal(torch.mm(x, linear.weight.data.T) + linear.bias.data, out))
         self.assertTrue(isinstance(out, GTensor))
         self.assertTrue(torch.equal(out.get_inputs()[0], x))
         grads = torch.ones((2, 4))
         out = linear.backward(grads)
         self.assertTrue(torch.equal(torch.mm(grads, linear.weight.data), out))
+
+    def test_convtranspose2d(self):
+        # Create a 3x3 input tensor
+        x = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+        # apply transpose convolution
+        torch_conv = torch.nn.ConvTranspose2d(1, 1, kernel_size=2, stride=1, padding=0, output_padding=0)
+        torch_conv.weight.data = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+        # apply kernel
+        torch_y = torch_conv(x)
+        # apply kernel with our implementation
+        conv = ConvTranspose2d(1, 1, kernel_size=2, stride=1, padding=0, output_padding=0)
+        conv.weight.data = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+        y = conv(x)
+        self.assertTrue(torch.equal(y, torch_y))
 
     def test_relu(self):
         x = torch.tensor([2.0, -5.0, 3.0, 0.0])
@@ -32,14 +48,15 @@ class TestModules(unittest.TestCase):
         x = torch.tensor([0, 1])
         sigmoid = Sigmoid()
         out = sigmoid.forward(x)
-        self.assertTrue(torch.equal(out, torch.tensor([0.5, 1 / (1 + 1/torch.e)])))
+        self.assertTrue(torch.equal(out, torch.tensor([0.5, 1 / (1 + 1 / torch.e)])))
         self.assertTrue(isinstance(out, GTensor))
         self.assertTrue(torch.equal(out.get_inputs()[0], x))
 
         grads = torch.ones((2,))
         out = sigmoid.backward(grads)
-        self.assertTrue(torch.equal(out, torch.tensor([0.25, (1/torch.e) / (1 + 1/torch.e) ** 2])))
-    
+        self.assertTrue(torch.equal(out, torch.tensor(
+            [0.25, (1 / torch.e) / (1 + 1 / torch.e) ** 2])))
+
     def test_sequential(self):
         x = torch.tensor([[1.0, 1.0], [1.0, 1.0]])
         model = Sequential(Linear(2, 4), ReLU(), Linear(4, 1))
@@ -51,13 +68,13 @@ class TestModules(unittest.TestCase):
         grads = torch.ones((2, 1))
         out = model.backward(grads)
         self.assertEqual(out.shape, (2, 2))
-    
+
     def test_mseloss(self):
         x = torch.tensor([2.0, -5.0, 3.0, 0.0])
         y = torch.tensor([2.0, 5.0, 4.0, 2.0])
         loss = MSELoss()
         out = loss(x, y)
-        self.assertTrue(torch.equal(out, torch.tensor(105/4)))
+        self.assertTrue(torch.equal(out, torch.tensor(105 / 4)))
         self.assertTrue(isinstance(out, GTensor))
         self.assertTrue(torch.equal(out.get_inputs()[0], x))
         self.assertTrue(torch.equal(out.get_inputs()[1], y))
